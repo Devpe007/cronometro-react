490d0359469a179e396d168d994b3e2e15a56365
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-31 22:48:32 -0300

    styling the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import './styles.css';
+
+import stopWatchImg from './assets/cronometro.png';
+
 class App extends React.Component {
   render() {
     return (
-      <div>
-        
+      <div className="container" >
+        <img className="img" src={stopWatchImg} alt="cronometro" />
+        <a className="timer" >0.0</a>
+
+        <div className="areaBtn" >
+          <a className="button" >VAI</a>
+          <a className="button" >LIMPAR</a>
+        </div>
       </div>
     );
   };
