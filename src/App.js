524acc13517153db3019e14dd3eadba097a3adce
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-31 22:57:28 -0300

    created the function to release and lock the timer

--- src/App.js
+++ src/App.js
@@ -5,15 +5,51 @@ import './styles.css';
 import stopWatchImg from './assets/cronometro.png';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 0,
+      button: 'VAI',
+    };
+
+    this.timer = null;
+  };
+
+  go = () => {
+    let state = this.state;
+
+    if(this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null
+      state.button = 'VAI';
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.number += 0.1;
+  
+        this.setState(state);
+      }, 100);
+
+      state.button = 'PAUSAR'
+    };
+
+    this.setState(state);
+  };
+
+  clear = () => {
+
+  };
+
   render() {
     return (
       <div className="container" >
         <img className="img" src={stopWatchImg} alt="cronometro" />
-        <a className="timer" >0.0</a>
+        <a className="timer" >{this.state.number.toFixed(1)}</a>
 
         <div className="areaBtn" >
-          <a className="button" >VAI</a>
-          <a className="button" >LIMPAR</a>
+          <a className="button"  onClick={this.go} >{this.state.button}</a>
+          <a className="button"  onClick={this.clear} >LIMPAR</a>
         </div>
       </div>
     );
